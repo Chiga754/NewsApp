821c63f66a6b4d44122ffddefc8a3f51185d9eab
Chiga754 <92618989+Chiga754@users.noreply.github.com>
2022-01-18 19:47:05 +0500

    Отображение новостей при загрузке страницы

--- app.js
+++ app.js
@@ -56,7 +56,9 @@ function customHttp() {
 // Init http module
 const http = customHttp();
 
+
+
 //  init selects
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   M.AutoInit();
 });
